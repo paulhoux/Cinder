c40ca475ac2c7bd7750ddee08378cf6d465f9721
Paul Houx <github@cowlumbus.nl>
2015-01-19 18:48:57 -0500

    WIP

--- include/cinder/evr/SharedTexture.h
+++ include/cinder/evr/SharedTexture.h
@@ -60,258 +60,280 @@ POSSIBILITY OF SUCH DAMAGE.
 //#pragma comment (lib,"evr.lib")
 
 namespace cinder {
-namespace msw {
-namespace video {
+	namespace msw {
+		namespace video {
 
-typedef std::unordered_map<GLuint, class SharedTexture> SharedTextureList;
-typedef std::deque<GLuint> SharedTextureIDList;
+			typedef std::unordered_map<GLuint, class SharedTexture> SharedTextureList;
+			typedef std::deque<GLuint> SharedTextureIDList;
 
-class SharedTexture {
-public:
-	SharedTexture()
-		: pD3DSurface( NULL ), pD3DTexture( NULL ), handle( NULL ), locked( FALSE ) {}
+			class SharedTexture {
+			public:
+				SharedTexture()
+					: m_hD3DSurface( NULL ), m_pD3DSurface( NULL ), m_pD3DTexture( NULL ), locked( FALSE ) {}
+				SharedTexture( IDirect3DDevice9Ex *pDevice, const D3DSURFACE_DESC &desc )
+				{
+					HRESULT hr = S_OK;
 
-	SharedTexture( const SharedTexture &other )
-		: SharedTexture()
-	{
-		name = other.name;
-		handle = other.handle;
-		value = other.value;
-		locked = other.locked;
+					do {
+						BREAK_ON_NULL( pDevice, E_POINTER );
 
-		pGLTexture = other.pGLTexture;
-		CopyComPtr( pD3DSurface, other.pD3DSurface );
-		CopyComPtr( pD3DTexture, other.pD3DTexture );
-	}
+						hr = pDevice->CreateOffscreenPlainSurface( desc.Width, desc.Height, desc.Format, D3DPOOL_DEFAULT, &m_pD3DSurface, &m_hD3DSurface );
+						BREAK_ON_FAIL( hr );
+					} while( false );
+				}
 
-	~SharedTexture()
-	{
-		SafeRelease( pD3DSurface );
-		SafeRelease( pD3DTexture );
-	}
+				SharedTexture( const SharedTexture &other )
+					: SharedTexture()
+				{
+					name = other.name;
+					value = other.value;
+					locked = other.locked;
 
-	SharedTexture& operator=( SharedTexture other )
-	{
-		std::swap( name, other.name );
-		std::swap( handle, other.handle );
-		std::swap( value, other.value );
-		std::swap( locked, other.locked );
+					m_hGLTexture = other.m_hGLTexture;
+					m_pGLTexture = other.m_pGLTexture;
 
-		std::swap( pGLTexture, other.pGLTexture );
-		std::swap( pD3DSurface, other.pD3DSurface );
-		std::swap( pD3DTexture, other.pD3DTexture );
+					m_hD3DSurface = other.m_hD3DSurface;
+					CopyComPtr( m_pD3DSurface, other.m_pD3DSurface );
+					CopyComPtr( m_pD3DTexture, other.m_pD3DTexture );
+				}
 
-		return *this;
-	}
+				~SharedTexture()
+				{
+					SafeRelease( m_pD3DSurface );
+					SafeRelease( m_pD3DTexture );
+				}
 
-	GLuint name;
-	HANDLE handle;
-	DWORD  value;
-	BOOL   locked;
+				SharedTexture& operator=( SharedTexture &&other )
+				{
+					std::swap( name, other.name );
+					std::swap( value, other.value );
+					std::swap( locked, other.locked );
 
-	gl::Texture2dRef   pGLTexture;
-	IDirect3DSurface9* pD3DSurface;
-	IDirect3DTexture9* pD3DTexture;
-};
+					std::swap( m_hGLTexture, other.m_hGLTexture );
+					std::swap( m_pGLTexture, other.m_pGLTexture );
 
-class SharedTexturePool {
-public:
-	~SharedTexturePool()
-	{
-		resetDevice();
-	}
+					std::swap( m_hD3DSurface, other.m_hD3DSurface );
+					std::swap( m_pD3DSurface, other.m_pD3DSurface );
+					std::swap( m_pD3DTexture, other.m_pD3DTexture );
 
-	static SharedTexturePool& instance()
-	{
-		static SharedTexturePool instance;
-		return instance;
-	}
+					return *this;
+				}
 
-	bool setDevice( IDirect3DDevice9Ex *pDevice )
-	{
-		assert( m_pDevice == NULL ); // Only allow the device to be set once.
-		assert( pDevice != NULL ); // Make sure a valid pointer was passed.
+				explicit 
 
-		m_pDevice = pDevice;
-		m_pDevice->AddRef();
+			private:
+				GLuint name;
+				DWORD  value;
+				BOOL   locked;
 
-		// Make sure OpenGL interop is initialized.
-		m_pD3DDeviceHandle = wglDXOpenDeviceNV( m_pDevice );
-		if( !m_pD3DDeviceHandle )
-			return false;
+				// OpenGL texture and handle
+				HANDLE             m_hGLTexture;
+				gl::Texture2dRef   m_pGLTexture;
 
-		return true;
-	}
+				// DirectX texture and handle
+				HANDLE             m_hD3DSurface;
+				IDirect3DSurface9* m_pD3DSurface;
+				IDirect3DTexture9* m_pD3DTexture;
+			};
 
-	void resetDevice()
-	{
-		// Release all textures.
-		mSharedTextureFreeIDs.clear();
-		mSharedTextures.clear();
+			class SharedTexturePool {
+			public:
+				~SharedTexturePool()
+				{
+					resetDevice();
+				}
 
-		// Close OpenGL interop.
-		if( m_pD3DDeviceHandle ) {
-			wglDXCloseDeviceNV( m_pD3DDeviceHandle );
-			m_pD3DDeviceHandle = NULL;
-		}
+				static SharedTexturePool& instance()
+				{
+					static SharedTexturePool instance;
+					return instance;
+				}
 
-		SafeRelease( m_pDevice );
-	}
+				bool setDevice( IDirect3DDevice9Ex *pDevice )
+				{
+					assert( m_pDevice == NULL ); // Only allow the device to be set once.
+					assert( pDevice != NULL ); // Make sure a valid pointer was passed.
 
-	bool createTexture( int width, int height );
+					m_pDevice = pDevice;
+					m_pDevice->AddRef();
 
-	SharedTexture& getOldestTexture( int width, int height )
-	{
-		// Make sure the D3D device was set & OpenGL interop is available.
-		assert( m_pDevice );
-		assert( m_pD3DDeviceHandle );
+					// Make sure OpenGL interop is initialized.
+					m_pD3DDeviceHandle = wglDXOpenDeviceNV( m_pDevice );
+					if( !m_pD3DDeviceHandle )
+						return false;
 
-		ScopedCriticalSection lock( mSharedTexturesLock );
+					return true;
+				}
 
-		// Check if there are free textures.
-		if( !mSharedTextureFreeIDs.empty() ) {
-			GLuint textureID = mSharedTextureFreeIDs.front();
-			mSharedTextureFreeIDs.pop_front();
-			return mSharedTextures[textureID];
-		}
+				void resetDevice()
+				{
+					// Release all textures.
+					mSharedTextureFreeIDs.clear();
+					mSharedTextures.clear();
 
-		// Create a new shared texture if there aren't.
-		if( createTexture( width, height ) ) {
-			GLuint textureID = mSharedTextureFreeIDs.front();
-			mSharedTextureFreeIDs.pop_front();
-			return mSharedTextures[textureID];
-		}
+					// Close OpenGL interop.
+					if( m_pD3DDeviceHandle ) {
+						wglDXCloseDeviceNV( m_pD3DDeviceHandle );
+						m_pD3DDeviceHandle = NULL;
+					}
 
-		return SharedTexture();
-	}
+					SafeRelease( m_pDevice );
+				}
 
-	void freeTexture( const SharedTexture& texture )
-	{
-		// Check if texture is part of this pool.
-		if( mSharedTextures.find( texture.name ) != mSharedTextures.end() ) {
-			mSharedTextureFreeIDs.push_back( texture.name );
-		}
-	}
+				bool createTexture( int width, int height );
 
-private:
-	SharedTexturePool() : m_pDevice( NULL ), m_pD3DDeviceHandle( NULL )
-	{
-		// Keep track of main thread.
-		mMainThreadID = std::this_thread::get_id();
+				SharedTexture& getOldestTexture( int width, int height )
+				{
+					// Make sure the D3D device was set & OpenGL interop is available.
+					assert( m_pDevice );
+					assert( m_pD3DDeviceHandle );
 
-		// Define the default texture format.
-		mFormat.setTarget( GL_TEXTURE_RECTANGLE );
-	}
+					ScopedCriticalSection lock( mSharedTexturesLock );
 
-private:
-	IDirect3DDevice9Ex    *m_pDevice;
-	HANDLE                 m_pD3DDeviceHandle;     // Shared device handle for OpenGL interop.
+					// Check if there are free textures.
+					if( !mSharedTextureFreeIDs.empty() ) {
+						GLuint textureID = mSharedTextureFreeIDs.front();
+						mSharedTextureFreeIDs.pop_front();
+						return mSharedTextures[textureID];
+					}
 
-	int                    mWidth;                 // Width of all shared textures.
-	int                    mHeight;                // Height of all shared textures.
+					// Create a new shared texture if there aren't.
+					if( createTexture( width, height ) ) {
+						GLuint textureID = mSharedTextureFreeIDs.front();
+						mSharedTextureFreeIDs.pop_front();
+						return mSharedTextures[textureID];
+					}
 
-	gl::Texture2d::Format  mFormat;
+					return SharedTexture();
+				}
 
-	CriticalSection        mSharedTexturesLock;
+				void freeTexture( const SharedTexture& texture )
+				{
+					// Check if texture is part of this pool.
+					if( mSharedTextures.find( texture.name ) != mSharedTextures.end() ) {
+						mSharedTextureFreeIDs.push_back( texture.name );
+					}
+				}
 
-	SharedTextureList      mSharedTextures;
-	SharedTextureIDList    mSharedTextureFreeIDs;
+			private:
+				SharedTexturePool() : m_pDevice( NULL ), m_pD3DDeviceHandle( NULL )
+				{
+					// Keep track of main thread.
+					mMainThreadID = std::this_thread::get_id();
 
-	std::thread::id        mMainThreadID;
-};
+					// Define the default texture format.
+					mFormat.setTarget( GL_TEXTURE_RECTANGLE );
+				}
 
-class ScopedSharedTexture {
-public:
-	ScopedSharedTexture( int width, int height )
-		: mTexture( SharedTexturePool::instance().getOldestTexture( width, height ) )
-	{
-	}
+			private:
+				IDirect3DDevice9Ex    *m_pDevice;
+				HANDLE                 m_pD3DDeviceHandle;     // Shared device handle for OpenGL interop.
 
-	~ScopedSharedTexture()
-	{
-		SharedTexturePool::instance().freeTexture( mTexture );
-	}
+				int                    mWidth;                 // Width of all shared textures.
+				int                    mHeight;                // Height of all shared textures.
 
-	SharedTexture& operator()() { return mTexture; }
+				gl::Texture2d::Format  mFormat;
 
-private:
-	SharedTexture& mTexture;
-};
+				CriticalSection        mSharedTexturesLock;
 
-inline bool SharedTexturePool::createTexture( int width, int height )
-{
-	// Only allow texture creation on the main thread.
-	if( std::this_thread::get_id() != mMainThreadID )
-		return false;
+				SharedTextureList      mSharedTextures;
+				SharedTextureIDList    mSharedTextureFreeIDs;
 
-	// Sanity check.
-	if( !mSharedTextures.empty() ) {
-		// Texture size should match.
-		assert( mWidth == width );
-		assert( mHeight == height );
-	}
-	else {
-		// Set texture size.
-		mWidth = width;
-		mHeight = height;
-	}
+				std::thread::id        mMainThreadID;
+			};
 
-	// Create OpenGL texture.
-	SharedTexture shared;
+			class ScopedSharedTexture {
+			public:
+				ScopedSharedTexture( int width, int height )
+					: mTexture( SharedTexturePool::instance().getOldestTexture( width, height ) )
+				{
+				}
 
-	shared.pGLTexture = gl::Texture2d::create( mWidth, mHeight, mFormat );
-	shared.name = shared.pGLTexture->getId();
+				~ScopedSharedTexture()
+				{
+					SharedTexturePool::instance().freeTexture( mTexture );
+				}
 
-	// Share it with DirectX.
-	HANDLE pSharedHandle = NULL;
-	HRESULT hr = m_pDevice->CreateTexture( width, height, 1, D3DUSAGE_RENDERTARGET, D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &( shared.pD3DTexture ), &pSharedHandle );
+				SharedTexture& operator()() { return mTexture; }
 
-	ULONG rc = GetRefCount( shared.pD3DTexture );
+			private:
+				SharedTexture& mTexture;
+			};
 
-	if( FAILED( hr ) || !pSharedHandle )
-		return false;
+			inline bool SharedTexturePool::createTexture( int width, int height )
+			{
+				// Only allow texture creation on the main thread.
+				if( std::this_thread::get_id() != mMainThreadID )
+					return false;
 
-	if( !wglDXSetResourceShareHandleNV( shared.pD3DTexture, pSharedHandle ) )
-		return false;
+				// Sanity check.
+				if( !mSharedTextures.empty() ) {
+					// Texture size should match.
+					assert( mWidth == width );
+					assert( mHeight == height );
+				}
+				else {
+					// Set texture size.
+					mWidth = width;
+					mHeight = height;
+				}
 
-	shared.handle = wglDXRegisterObjectNV( m_pD3DDeviceHandle, shared.pD3DTexture, shared.name, GL_TEXTURE_RECTANGLE, WGL_ACCESS_READ_ONLY_NV );
-	if( !shared.handle ) {
-		switch( GetLastError() ) {
-		case ERROR_INVALID_HANDLE:
-			//CI_LOG_E( "No GL context is made current to the calling thread." );
-			break;
-		case ERROR_INVALID_DATA:
-			//CI_LOG_E( "Incorrect <name> <type> or <access> parameters." );
-			break;
-		case ERROR_OPEN_FAILED:
-			//CI_LOG_E( "Opening the Direct3D resource failed." );
-			break;
-		default:
-			//CI_LOG_E( "Failed to register objects: " << GetLastError() );
-			break;
-		}
-		return false;
-	}
+				// Create OpenGL texture.
+				SharedTexture shared;
 
-	shared.pD3DTexture->GetSurfaceLevel( 0, &( shared.pD3DSurface ) );
+				shared.pGLTexture = gl::Texture2d::create( mWidth, mHeight, mFormat );
+				shared.name = shared.pGLTexture->getId();
 
-	rc = GetRefCount( shared.pD3DTexture );
-	rc = GetRefCount( shared.pD3DSurface );
+				// Share it with DirectX.
+				HANDLE pSharedHandle = NULL;
+				HRESULT hr = m_pDevice->CreateTexture( width, height, 1, D3DUSAGE_RENDERTARGET, D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &( shared.pD3DTexture ), &pSharedHandle );
 
-	// Now lock it once, it's an expensive operation. We'll promise to make sure we're not writing to the texture while OpenGL is using it.
-	if( wglDXLockObjectsNV( m_pD3DDeviceHandle, 1, &( shared.handle ) ) ) {
-		// Store texture in map and make it available for use.
-		mSharedTextures[shared.name] = shared;
-		mSharedTextureFreeIDs.push_back( shared.name );
+				ULONG rc = GetRefCount( shared.pD3DTexture );
 
-		return true;
-	}
+				if( FAILED( hr ) || !pSharedHandle )
+					return false;
 
-	return false;
-}
+				if( !wglDXSetResourceShareHandleNV( shared.pD3DTexture, pSharedHandle ) )
+					return false;
 
-} // namespace video
-} // namespace msw
+				shared.handle = wglDXRegisterObjectNV( m_pD3DDeviceHandle, shared.pD3DTexture, shared.name, GL_TEXTURE_RECTANGLE, WGL_ACCESS_READ_ONLY_NV );
+				if( !shared.handle ) {
+					switch( GetLastError() ) {
+					case ERROR_INVALID_HANDLE:
+						//CI_LOG_E( "No GL context is made current to the calling thread." );
+						break;
+					case ERROR_INVALID_DATA:
+						//CI_LOG_E( "Incorrect <name> <type> or <access> parameters." );
+						break;
+					case ERROR_OPEN_FAILED:
+						//CI_LOG_E( "Opening the Direct3D resource failed." );
+						break;
+					default:
+						//CI_LOG_E( "Failed to register objects: " << GetLastError() );
+						break;
+					}
+					return false;
+				}
+
+				shared.pD3DTexture->GetSurfaceLevel( 0, &( shared.pD3DSurface ) );
+
+				rc = GetRefCount( shared.pD3DTexture );
+				rc = GetRefCount( shared.pD3DSurface );
+
+				// Now lock it once, it's an expensive operation. We'll promise to make sure we're not writing to the texture while OpenGL is using it.
+				if( wglDXLockObjectsNV( m_pD3DDeviceHandle, 1, &( shared.handle ) ) ) {
+					// Store texture in map and make it available for use.
+					mSharedTextures[shared.name] = shared;
+					mSharedTextureFreeIDs.push_back( shared.name );
+
+					return true;
+				}
+
+				return false;
+			}
+
+		} // namespace video
+	} // namespace msw
 } // namespace cinder
 
 #endif // CINDER_MSW
