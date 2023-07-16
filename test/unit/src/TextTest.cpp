4c05079469d507116aebc43cdd97568ea9f56188
Andrew Bell <cinder.afb@gmail.com>
2023-07-16 17:33:44 -0400

    Unit: TextTests - first tests

--- test/unit/src/TextTest.cpp
+++ test/unit/src/TextTest.cpp
@@ -13,15 +13,75 @@
 using namespace ci;
 using namespace std;
 
+size_t countRuns( const text::AttrString &as )
+{
+	text::AttrStringIter iter = as.iterate( text::loadFont( text::systemDefaultFace(), 12 ) );
+	size_t runCount = 0;
+	while( iter.nextRun() )
+		runCount++;
+
+	return runCount;
+}
+
+bool verifyAlignmentRuns( const text::AttrString &as, const vector<pair<text::Alignment, size_t>> &expectedRuns )
+{
+	text::AttrStringIter iter = as.iterate( text::loadFont( text::systemDefaultFace(), 12 ) );
+	size_t runCount = 0;
+	while( iter.nextRun() ) {
+		if( runCount >= expectedRuns.size() )
+			return false;
+		if( expectedRuns[runCount].first != iter.getAlignment( text::Alignment::DEFAULT ) || expectedRuns[runCount].second != iter.getLengthCh() )
+			return false;
+		runCount++;
+	}
+
+	return runCount == expectedRuns.size();
+}
+
 TEST_CASE("Text")
 {
 	SECTION("AttrString")
 	{
 		text::AttrString as1;
-		REQUIRE( (as1.size() == 0 && as1.empty()) );
+		CHECK( (as1.size() == 0 && as1.empty()) );
 		as1 << "hello";
-		REQUIRE( (as1.size() == 5 && ! as1.empty()) );
+		CHECK( (as1.size() == 5 && ! as1.empty()) );
+		CHECK( countRuns( as1 ) == 1 );
 		as1.clear();
-		REQUIRE( (as1.size() == 0 && as1.empty()) );
+		CHECK( (as1.size() == 0 && as1.empty()) );
+		CHECK( countRuns( as1 ) == 0 );
+
+		text::AttrString as2;
+		// single attr
+		as2 << text::Leading::mult( 1.2f ) << "Hello";
+		CHECK( countRuns( as2 ) == 1 );
+		// trailing attr doesn't create extra run
+		as2 << text::Leading::mult( 1.3f );
+		CHECK( countRuns( as2 ) == 1 );
+		// but appending text creates second run
+		as2 << " world";
+		CHECK( countRuns( as2 ) == 2 );
+
+		// check alignment runs
+		text::AttrString as3;
+		as3 << "A";
+		CHECK( countRuns( as3 ) == 1 );
+		CHECK( verifyAlignmentRuns( as3, { { text::Alignment::DEFAULT, 1 } } ) );
+		as3 << text::Alignment::CENTER;
+		CHECK( verifyAlignmentRuns( as3, { { text::Alignment::DEFAULT, 1 } } ) );
+		as3 << "AA";		
+		CHECK( verifyAlignmentRuns( as3, { { text::Alignment::DEFAULT, 1 }, { text::Alignment::CENTER, 2 } } ) );
+		as3 << text::Alignment::RIGHT << "AAA";
+		CHECK( verifyAlignmentRuns( as3, { { text::Alignment::DEFAULT, 1 }, { text::Alignment::CENTER, 2 }, {text::Alignment::RIGHT, 3 } } ) );
+		as3 << "A";
+		CHECK( verifyAlignmentRuns( as3, { { text::Alignment::DEFAULT, 1 }, { text::Alignment::CENTER, 2 }, {text::Alignment::RIGHT, 4 } } ) );
+
+		// check RunBreaks
+		text::AttrString as4;
+		as4 << "A" << text::RunBreak() << "B";
+		CHECK( countRuns( as4 ) == 2 );
+		// trailing RunBreak should not result in an additional Run
+		as4 << text::RunBreak();
+		CHECK( countRuns( as4 ) == 2 );
 	}
 }
