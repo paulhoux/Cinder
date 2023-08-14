e870f0378df91c1dadb9569da73e10a6becf4608
Andrew Bell <cinder.afb@gmail.com>
2023-08-13 22:23:42 -0400

    Some cleanup to CairoText demo

--- samples/_text/CairoText/src/CairoTextApp.cpp
+++ samples/_text/CairoText/src/CairoTextApp.cpp
@@ -17,51 +17,15 @@ using namespace ci::app;
 
 text::Face *gFace;
 
-class TextFragment {
-  public:
-	TextFragment( vec2 loc, ColorA color )
-		: mLoc( loc ), mColor( color )
-	{}
-	
-	void draw( cairo::Context &ctx ) const
-	{
-		int32_t canvasWidth = getWindowWidth();
-		int32_t canvasHeight = getWindowHeight();
-
-		ci::Path2d path;
-		path.moveTo( vec2( 0, 0 ) );
-		//path.lineTo( canvasWidth, canvasHeight );
-		path.curveTo( vec2( 200, -200 ), vec2( canvasWidth / 2, 200 ), vec2( canvasWidth, 20 ) );
-		path.curveTo( vec2( 200, -200 ), vec2( 300, 200 ), vec2( canvasWidth - 20, 120 ) );
-		text::AttrString str;
-		str << text::loadFont( gFace, 48.0f ) << Color( 1.0f, 0.25f, 1.0f ) << "Jos\xc3\xa9 and Zo\xc3\xab enjoyed caf\xc3\xa9 cr\xc3\xa8me and"
-			<< text::loadFont( gFace, 64.0f ) << Color( 0.5f, 0.25f, 1.0f ) << " affogatos in Malm\xc3\xb6.";
-
-		ctx.setSource( Color( 0, 0, 1.0f ) );
-		ctx.save();
-		ctx.translate( mLoc );
-		ctx.appendPath( path );
-		ctx.stroke();
-		ctx.restore();
-
-		ctx.showText( text::TextOnPath( str, path, text::TypesetOptions(), getElapsedSeconds() * 0, -getElapsedSeconds() * 2 ), mLoc );
-		//ctx.showText( text::Frame( str ), mLoc );	
-	};
-	
-  private:
-	vec2		mLoc;
-	ColorA		mColor;
-};
-
 class CairoTextApp : public App {
   public:
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
 	void keyDown( KeyEvent event ) override;
-	void renderScene( cairo::Context &ctx );
+	void renderTextOverlay( cairo::Context& ctx ) const;
+	void renderTextOnPath( cairo::Context& ctx ) const;
+	void renderScene( cairo::Context& ctx );
 	void draw() override;
-	
-	vector<TextFragment>		mTextFragments;
 };
 
 void CairoTextApp::setup()
@@ -72,19 +36,11 @@ void CairoTextApp::setup()
 
 void CairoTextApp::mouseDown( MouseEvent event )
 {	
-	// create a new TextFragment
-	mTextFragments.push_back( TextFragment( event.getPos(), ColorA( CM_HSV, randFloat(), 1, 1, 0.65f ) ) );
 }
 
 void CairoTextApp::keyDown( KeyEvent event )
 {
-	if( event.getCode() == KeyEvent::KEY_f ) {
-		setFullScreen( ! isFullScreen() );
-	}
-	else if( event.getChar() == 'x' ) {
-		mTextFragments.clear();
-	}
-	else if( event.getChar() == 's' ) {
+	if( event.getChar() == 's' ) {
 		cairo::Context ctx( cairo::SurfaceSvg( getHomeDirectory() / "CairoBasicShot.svg", getWindowWidth(), getWindowHeight() ) );
 		renderScene( ctx );
 	}
@@ -102,6 +58,49 @@ void CairoTextApp::keyDown( KeyEvent event )
 	}	
 }
 
+void CairoTextApp::renderTextOverlay( cairo::Context& ctx ) const
+{
+	text::Frame frame( text::AttrString() << text::font( gFace, 384.0f ) << "Cinder" );
+	float xOffset = (getWindowWidth() - frame.getMeasuredWidth()) / 2;
+	ctx.glyphPath( frame, vec2{ xOffset, getWindowCenter().y / 2} );
+
+	// Fill the text path with the gradient
+	cairo::GradientLinear gradient{ {xOffset, 0}, { xOffset + frame.getMeasuredWidth(), 0} };
+	gradient.addColorStop( 0.0, ColorA( 1.0f, 0.5f, 0.25f, 0.2f ) );
+	gradient.addColorStop( 1.0, ColorA( 1.0f, 0.9f, 0.1f, 0.4f ) );
+	ctx.setSource( gradient );
+	ctx.fillPreserve();
+	ctx.setSource( ColorA( 0.8f, 0.1f, 0.1f, 0.8f ) );
+	ctx.stroke();
+}
+
+void CairoTextApp::renderTextOnPath( cairo::Context& ctx ) const
+{
+	int32_t canvasWidth = getWindowWidth();
+	int32_t canvasHeight = getWindowHeight();
+	vec2 drawOffset{ 50, canvasHeight / 2.0f };
+
+	ci::Path2d path;
+	path.moveTo( vec2( 0, 0 ) );
+	//path.lineTo( canvasWidth, canvasHeight );
+	path.curveTo( vec2( 200, -200 ), vec2( canvasWidth / 2, 200 ), vec2( canvasWidth, 20 ) );
+	path.curveTo( vec2( canvasWidth - 50, -200 ), vec2( 300, 200 ), vec2( canvasWidth - 50, 120 ) );
+	text::AttrString str;
+	str << text::font( gFace, 48.0f ) << Color( 1.0f, 0.25f, 1.0f ) << "Jos\xc3\xa9 and Zo\xc3\xab enjoyed caf\xc3\xa9 cr\xc3\xa8me and"
+		<< text::font( gFace, 64.0f ) << Color( 0.5f, 0.25f, 1.0f ) << " affogatos in Malm\xc3\xb6.";
+
+	// draw the path itself
+	ctx.setSource( Color( 0, 0, 1.0f ) );
+	ctx.save();
+	ctx.translate( drawOffset );
+	ctx.appendPath( path );
+	ctx.stroke();
+	ctx.restore();
+
+	ctx.showText( text::TextOnPath( str, path, text::TypesetOptions(), getElapsedSeconds() * 0, -getElapsedSeconds() * 0 ), drawOffset );
+	//ctx.showText( text::Frame( str ), mLoc );	
+}
+
 void CairoTextApp::renderScene( cairo::Context &ctx )
 {
 	// clear the context with our radial gradient
@@ -110,13 +109,9 @@ void CairoTextApp::renderScene( cairo::Context &ctx )
 	radialGrad.addColorStop( 1, Color( 0.6, 0.6, 0.6 ) );	
 	ctx.setSource( radialGrad );	
 	ctx.paint();
-	
-	ctx.setSource( Colorf( 1.0f, 0.5f, 0.25f ) );
-	ctx.setFont( loadFont( gFace, 24 ) );
-	ctx.showText( "12345" );
 
-	for( vector<TextFragment>::const_iterator flIt = mTextFragments.begin(); flIt != mTextFragments.end(); ++flIt )
-		flIt->draw( ctx );
+	renderTextOnPath( ctx );
+	renderTextOverlay( ctx );
 }
 
 void CairoTextApp::draw()
